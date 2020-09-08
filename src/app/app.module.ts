de3d8043f519426929276376d1e3ab3d355dd478
dev <dev@kierans-portfolio.attlocal.net>
2020-09-08 09:37:57 -0500

    finished, check routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// 1. Import FormsModule 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +19,10 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // 2. Add FormsModule to the imports list
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
